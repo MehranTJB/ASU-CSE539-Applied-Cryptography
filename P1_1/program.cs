440c089e9fbf9c47e07953a7b234cfc670fa3a13
Mehran Tajbakhsh <github@mehrantajbakhsh.com>
2022-05-09 16:42:55 -0700

    Course assignments

---
+++ P1_1/program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace P1_1
+{
+    class Program
+    {
+        public static byte[] StegFunc(byte[] InputBytes, byte[] bmpImgBytes)
+        {
+            BitArray InputBits = new BitArray(InputBytes);
+
+            byte[] TempByteArray = new byte[bmpImgBytes.Length]; // just a placeholder so that the code works from scatch without errors
+            
+            for(int i = 0; i < 26; i++)
+            {
+                TempByteArray[i] = bmpImgBytes[i];
+            }
+
+            for(int i = 0; i < 96; i += 2)
+            {
+                Byte Counter;
+                int ImgZoneMax = (i / 8 + 1) * 8 - 1;
+                int ImgZoneMin = (i / 8) * 8;
+                int ImageZone = ImgZoneMax - (i - ImgZoneMin);
+                if(InputBits[ImageZone] == true && InputBits[ImageZone - 1] == true)
+                    Counter = 3;
+                else if(InputBits[ImageZone] == true && InputBits[ImageZone - 1] == false)
+                    Counter = 2;
+                else if(InputBits[ImageZone] == false && InputBits[ImageZone - 1] == true)
+                    Counter = 1;
+                else Counter = 0;
+                TempByteArray[i / 2 + 26] = BitConverter.GetBytes(bmpImgBytes[i / 2 + 26] ^ Counter)[0];
+            }
+
+            return TempByteArray;
+        }
+
+        public static string getInputFromCommandLine(string[] args)
+        {
+            string input = "";
+            if (args.Length == 1)
+            {
+                input = args[0]; // Gets the first string after the 'dotnet run' command
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input! use 'dotnet run' followed by a string of array HEX code");
+            }
+            return input;
+        }
+
+
+        public static string P1_1(string[] args)
+        {
+            byte[] bmpImgBytes = new byte[]
+            {
+                0x42,0x4D,0x4C,0x00,0x00,0x00,0x00,0x00,
+                0x00,0x00,0x1A,0x00,0x00,0x00,0x0C,0x00,
+                0x00,0x00,0x04,0x00,0x04,0x00,0x01,0x00,0x18,0x00,
+                0x00,0x00,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0xFF,
+                0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0x00,0x00,0x00,
+                0xFF,0xFF,0xFF,0x00,0x00,0x00,0xFF,0x00,0x00,
+                0xFF,0xFF,0xFF,0xFF,0x00,0x00,0xFF,0xFF,0xFF,
+                0xFF,0xFF,0xFF,0x00,0x00,0x00,0xFF,0xFF,0xFF,
+                0x00,0x00,0x00};
+
+            string Input = getInputFromCommandLine(args);
+
+        
+            string[] InputArray=Input.Split(' ');
+            byte[] InputBytes = new byte[12]; 
+
+            for(int i = 0; i < InputArray.Length; i++)
+            {
+                InputBytes[i] = Convert.ToByte(InputArray[i], 16);
+            }
+            
+            byte[] Result = StegFunc(InputBytes, bmpImgBytes); 
+
+            string OutputResult = BitConverter.ToString(Result).Replace("-", " "); 
+            Console.WriteLine(OutputResult); 
+
+            return OutputResult; 
+        }
+
+        static void Main(string[] args)
+        {   
+            P1_1(args);            
+        }
+
+    }
+}
+
---
+++ P4/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Numerics; 
+using System.Collections.Generic;
+
+namespace P4
+{
+    class Program
+    {   
+        // Find Greaest Common Divisor by Euclidean Algorithm
+	// 1: Let a, b be the two numbers
+	// 2. a mod b = R
+	// 3. Let a = b and b = R
+	// 4. Repeat Steps 2 , 3 until a mod b is greater than 0
+	// 5. Greatest Common Divisor = b
+	public static BigInteger Greatest_Common_Divisor(BigInteger Divisor , BigInteger Dividend)
+        {
+	    // ed = 1 mod (p - 1)(q - 1)
+	    // e -> a
+	    // phi_n (p -1)(q - 1) -> b
+            BigInteger Temp1 = 0, Temp2 = 0, Temp3 = 1, Temp4 = 1, Temp5 = 1;
+            BigInteger Quotient = 0, Remainder = 0, Grst_Comm_Div = 0;
+	    while(Divisor != 0)
+            {
+                Quotient = Dividend / Divisor;
+                Remainder = Dividend % Divisor;
+                Temp1 = Grst_Comm_Div - Temp4 * Quotient;
+                Temp2 = Temp3 - Temp5 * Quotient;
+                Dividend = Divisor;
+                Divisor = Remainder;
+                Grst_Comm_Div = Temp4;
+                Temp3 = Temp5;
+                Temp4 = Temp1;
+                Temp5 = Temp2;
+            }
+            return Grst_Comm_Div;
+        } 
+        public static string P4(string[] args)
+        {
+
+            BigInteger e = 65537;
+            int p_e = 0, p_c = 0, q_e = 0, q_c = 0;
+            BigInteger CipherText = 0;
+            BigInteger PlaintText = 0;
+            int Index = 1;
+            foreach(var Item in args)
+            {   
+                switch (Index)
+                {
+                    case 1:
+                        p_e = int.Parse(Item);
+                        break;
+                    case 2:
+                        p_c = int.Parse(Item);
+                        break;
+                    case 3:
+                        q_e = int.Parse(Item);
+                        break;
+                    case 4:
+                        q_c = int.Parse(Item);
+                        break;
+                    case 5:
+                        CipherText = BigInteger.Parse(Item);
+                        break;
+                    case 6:
+                        PlaintText = BigInteger.Parse(Item);
+                        break;
+                    default:
+                        break;
+                }
+                Index++;
+            }
+            // ed = 1 mod (p - 1)(q - 1)
+            // e -> a
+            // phi_n (p -1)(q - 1) -> b
+
+            BigInteger p_Prime = 0, q_Prime = 0;
+            p_Prime = BigInteger.Subtract(BigInteger.Pow(2, p_e), p_c);
+            q_Prime = BigInteger.Subtract(BigInteger.Pow(2, q_e), q_c);
+            BigInteger phi_n = BigInteger.Multiply(p_Prime - 1, q_Prime - 1);
+            BigInteger Exp = Greatest_Common_Divisor(e, phi_n);
+            
+	    BigInteger Result1 = BigInteger.ModPow(CipherText, Exp, p_Prime * q_Prime);
+            
+	    BigInteger Result2 = BigInteger.ModPow(PlaintText, e, p_Prime * q_Prime);
+            
+	    string Result = Result1.ToString() + "," + Result2.ToString();
+            Console.WriteLine(Result);
+            return Result;
+        }
+
+        static void Main(string[] args)
+        {
+            P4(args); 
+        }
+    }
+}
+
